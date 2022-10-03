8da9cdad183d81020af2b9c87acfd50d9e0a7acc
Marina <likholetova.marina@gmail.com>
2022-10-03 18:05:28 +0300

    cat

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import logo from './cat.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [arr, setArr] = useState([]);
+  
+  const like = arr.map((_, index) => {
+    return <div key={index} className='like-icon'></div>
+  });
+
+  function handleClick(event) {
+    setArr([...arr, 1]);
+
+    setTimeout(() => { 
+      if (arr.length) setArr([]);
+    }, 800);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        {like}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className='App-button' onClick={handleClick}>Feed</button>
       </header>
     </div>
   );
